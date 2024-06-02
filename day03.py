8eba597cf5e0de47e06aaa341eea38a8ac138c5f
Ahsan2001 <ahsan.sabir@yahoo.com>
2024-06-02 23:14:56 +0500

    practice sunday

---
+++ day03.py
@@ -0,0 +1,13 @@
+# convert fahrenheit into celsius 
+
+Fahrenheit  = int(input("Enter temperature in Fahrenheit  "))
+Celsius = (Fahrenheit  -32) * 5/9
+print(f"The temperature in celsius {Celsius:.2f}")
+
+
+# variable 
+# user input 
+# type conversion
+# arthmetic calculation
+# convert decimal into 2 decimal fraction
+# print with template literals 
