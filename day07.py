c439bcd5ec711d59d85b07416db0fe1006681e65
Ahsan2001 <ahsan.sabir@yahoo.com>
2024-06-25 08:58:24 +0500

    update last class code

---
+++ day07.py
@@ -0,0 +1,31 @@
+# matric_pass = True
+# test_clear = True
+
+# if (matric_pass and test_clear):
+#     print("you can take Admission")
+# else:
+#     print("youre in waiting list") 
+
+
+# countries = ["Pakistan", "India", "Turkey", "Indonesia"]
+
+# print(countries[-3:-1])
+
+
+# Greetings: Start with the list you used in Exercise 3-1, but instead of just 
+# printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the 
+# person’s name
+
+names = ["Ahsan", "Shahbaz", "Ali", "Qasim"]
+
+for name in names:
+    print(f"Hello {name} how are you doing today")
+
+names.pop(2)
+print(names)
+
+
+
+
+
+
