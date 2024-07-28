4330a06bcf63185ec85b965c588fd4c1e0b09138
M. Ahsan Shaikh <ahsan.sabir@yahoo.com>
2024-07-28 10:33:06 +0500

    completed task

--- task-employee-performance-evaluation.py
+++ task-employee-performance-evaluation.py
@@ -16,7 +16,7 @@
 
 # employee names, sales figures, and customer  satisfaction ratings
 
-employees =  [
+employeesData =  [
   ("John", 50000, 4.5),
   ("Alice", 60000, 4.7),
   ("Bob", 45000, 4.2),
@@ -30,3 +30,31 @@ employees =  [
 # and customer satisfaction ratings
 sales_weight = 0.6
 satisfaction_weight = 0.4
+
+
+
+
+
+employees = [
+    ("John", 50000, 4.5),
+    ("Alice", 60000, 4.7),
+    ("Bob", 45000, 4.2),
+    ("Emma", 55000, 4.6),
+    ("James", 48000, 4.4)
+]
+
+def evaluate_performance(data):
+    performance_list = []
+    
+    for x in data:  
+        calculating = (x[1] * 0.6 + x[2] * 0.4)
+        performance_list.append((x[0], calculating))
+    
+  
+    performance_list.sort(key=lambda y: y[1], reverse=True)
+    
+    for name, performance in performance_list:
+        print(f"Performance of {name} is {performance}")
+
+evaluate_performance(employees)
+
