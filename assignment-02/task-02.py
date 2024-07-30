8091ad785ef2f5f839135096aab48614601a596f
M. Ahsan Shaikh <ahsan.sabir@yahoo.com>
2024-07-31 03:48:32 +0500

    task 1 completed

---
+++ assignment-02/task-02.py
@@ -0,0 +1,4 @@
+# Scenario 2: Student Grades
+# You are managing student grades using a dictionary. You need to calculate the average grade.take
+# atleast 5 Students grades & then calculate the average.
+
---
+++ assignment-02/task-04.py
@@ -0,0 +1,5 @@
+# Scenario 3: Password Strength Checker
+# You are creating a program to check the strength of passwords based on certain criteria.
+# • Password should be at least 8 characters long.
+# • Password should contain at least one digit.
+# • Password should contain at least one letter
